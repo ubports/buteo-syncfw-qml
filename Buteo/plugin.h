ed3b2fd77da3f94693f2fc9ce85b3beac2f2bdc3
Renato Araujo Oliveira Filho <renato.filho@canonical.com>
2015-07-09 10:15:07 -0300

    Fixed header license.

--- Buteo/plugin.h
+++ Buteo/plugin.h
@@ -1,13 +1,11 @@
 /*
- * Copyright 2015 Canonical Ltd.
+ * Copyright (C) 2015 Canonical, Ltd.
  *
- * This file is part of buteo-sync-qml-plugin.
- *
- * sync-monitor is free software; you can redistribute it and/or modify
+ * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; version 3.
  *
- * contact-service-app is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
